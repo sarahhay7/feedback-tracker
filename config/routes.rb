7dc43bd659bf073c527ab9b292467078fd37f059
Pete Nicholls <pete@metanation.com>
2017-02-02 16:39:21 +1300

    Add integration sync buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'feedbacks#new'
   resources :feedbacks, only: [:new, :create]
   resources :feedback_states, only: :show
+
+  post 'integrations/:id/sync' => 'integration_syncs#create', as: :integration_sync
 end
